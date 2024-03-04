7e94b1b84f1ab283e6cbc0859adf4c0a3311115e
Harisivaram <153615137+Harisivaram@users.noreply.github.com>
2024-03-04 10:56:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function(){
+    // Instantiate Typed.js when the DOM is fully loaded
+    let typed = new Typed('#changing-text', {
+        strings: ['...', 'Front-end Developer', 'UI/UX'],
+        typeSpeed: 50,
+        backSpeed: 50,
+        backDely: 1000,
+        loop: true,
+        
+   
+    });
+});
+document.getElementById('hir-btn').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/harisivaram-r', '_blank');
+});
+document.getElementById('tms-btn').addEventListener('click', function() {
+    window.open('https://harisivaram.github.io/Tms/', '_blank');
+});
+document.getElementById('log-btn').addEventListener('click', function() {
+    window.open('https://github.com/Harisivaram/login.io/', '_blank');
+});
+
+document.getElementById('linkedin-btn').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/harisivaram-r', '_blank');
+});
+
+
